f5ccb886a3f51d60f99b699802e097c455314e01
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-09-07 12:26:51 +0200

    add logo.png

---
+++ php/header.php
@@ -0,0 +1,8 @@
+<header>
+  <section>
+    <h1>Kenny & Clément</h1>
+    <img src="../img/logo.png" alt="logo">
+  </section>
+  <input type="text" name="" value="" placeholder="recherche">
+<p> <?php echo date("H : i"); ?></p>
+</header>
