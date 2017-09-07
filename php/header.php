73048d3673f729b5e686bfee28fdc530cf322216
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-09-07 16:20:23 +0200

    before merge

---
+++ php/header.php
@@ -0,0 +1,8 @@
+<header class="d-flex ">
+  <section class="col-2 ">
+    <h1 >Kenny & Clément</h1>
+    <img class="w-100" src="img/logo.png" alt="logo">
+  </section>
+  <input type="text" name="" value="" placeholder="recherche" class="col-6 ">
+<p class="col-1 hidden-xs-down"> <?php echo date("H : i"); ?> </p>
+</header>
---
+++ php/tableauProduit.php
@@ -0,0 +1,41 @@
+<?php
+$produits = [
+["name"=> "Gigabyte GT 1030 Silent Low Profile 2G",
+"accroche" => "La carte graphique Gigabyte GT 1030 Silent Low Profile 2G est la carte graphique multimédia par excellence. Facile à intégrer, consommant peu d'énergie, elle est idéale pour doter votre PC de fonctionnalités multimédia avancées. Plus performante qu'un iGP standard, elle offre un affichage fluide.",
+"description" =>"La carte graphique Gigabyte GT 1030 Silent Low Profile 2G est la carte graphique multimédia par excellence. Facile à intégrer, consommant peu d'énergie, elle est idéale pour doter votre PC de fonctionnalités multimédia avancées. Plus performante qu'un iGP standard (puce graphique intégrée au processeur central), elle offre un affichage fluide en Haute Définition et vous permettra même de jouer à des jeux casual ou issus du Windows Store. <br/>
+Oubliez votre vieux chipset intégré : avec la GeForce GT 1030, surfez sur le Web à la vitesse de l'éclair, éditez facilement vos photos et vidéos, et profitez de jeux encore plus beaux ! La carte graphique dédiée NVIDIA GeForce GT 1030 délivre des performances graphiques plus rapides qu'avec un chipset intégré, sans oublier une fiabilité et une stabilité à toute épreuve avec l'application GeForce Experience. ",
+"source" =>"img/produit1.jpg",
+"prix" => "72€95"],
+
+["name"=> "Thermaltake Core V51 Tempered Glass Edition",
+"accroche" => "L'élégant boîtier Thermaltake Core V51 Tempered Glass Edition est un modèle de grande qualité avec une fenêtre en verre trempé. Son espace intérieur flexible offre une grande liberté pour l'installation des composants et vous offre aussi un grand potentiel de refroidissement même en watercooling.",
+"description" =>"L'élégant boîtier Thermaltake Core V51 Tempered Glass Edition est un modèle de grande qualité avec une fenêtre en verre trempé. Son espace intérieur flexible offre une grande liberté pour l'installation des composants et vous offre aussi un grand potentiel de refroidissement, y compris via watercooling. <br/>
+</br/>Le boîtier Thermaltake Core V51 TG dispose de nombreux espaces pour ventilateurs et permet l'installation d'un système de refroidissement liquide (watercooling). De plus, un système astucieux de gestion des câbles permet d'optimiser le flux d'air. Grâce à son grand potentiel de refroidissement, c'est un boîtier idéal pour les configurations haut de gamme." ,
+"source" =>"img/produit2.jpg",
+"prix" => "139€95"],
+
+["name"=> "ASRock Beebox J4205/B/BB Noir",
+"accroche" => "Laissez-vous séduire par la compacité, la vitesse et le silence du ASRock Beebox ! Basé sur une carte mère Intel NUC, ce mini PC offre une compatibilité 4K, permet un affichage simultané sur 3 écrans. C'est aussi le premier mini PC Intel NUC a disposer d'un connecteur USB Type C !",
+"description" =>"Laissez-vous séduire par la compacité, la vitesse et le silence du ASRock Beebox ! Basé sur une carte mère Intel NUC, ce mini PC offre une compatibilité 4K, permet un affichage simultané sur 3 écrans. C'est aussi le premier mini PC Intel NUC a disposer d'un connecteur USB Type C ! Parfaitement discret, l'ASRock Beebox est un achat idéal pour un large éventail d'utilisations : Home Cinéma, PC de bureau, PC de salon/chambre. <br />Le concept de la Beebox se base sur l'abeille. Tous deux sont de petite taille mais très rapides ! La Beebox ASRock concentre une carte mère animée par un SoC Intel J4205 et les autres périphériques indispensables à son fonctionnement dans un châssis ultra-compact qui reprend les caractéristiques de l'abeille... le bourdonnement en moins ! La Beebox est tellement silencieuse que vous pouvez la laisser allumée dans une chambre, lorsque vous dormez !" ,
+"source" =>"img/produit3.jpg",
+"prix" => "207€95"],
+
+["name"=> "Thermaltake View 71 TG RGB",
+"accroche" => "Profitez des nombreuses qualités du boîtier Grand Tour Thermaltake View 71 TG RGB pour mettre au point une configuration performante. Compatible watercooling et disposant déjà de 3 ventilateurs de 140 mm RGB, il offrira un environnement idéal pour vos composants.",
+"descrption" =>"Profitez des nombreuses qualités du boîtier Grand Tour Thermaltake View 71 TG RGB pour mettre au point une configuration performante. Compatible watercooling et disposant déjà de 3 ventilateurs de 140 mm avec rétro-éclairage RGB, il offrira un environnement idéal pour vos composants. Il est équipé de deux fenêtres en verre trempé et sera parfait pour installer vos cartes mères Mini-ITX, Micro ATX, ATX ou encore E-ATX. <br />Ce boîtier Grand Tour offre un espace intéressant pour une configuration complète et performante. Il est doté de deux fenêtres en verre trempé de 4 mm d'épaisseur pour un aperçu excellent. Il comprend également trois filtres à poussière magnétiques pour une protection idéale de votre équipement." ,
+"source" =>"img/produit4.jpg",
+"prix" => "224€95"],
+
+["name"=> "Synology DiskStation DS216play + Google Chromecast OFFERT !",
+"accroche" => "Pour l'achat d'un NAS Synology DiskStation DS216play, un Google Chromecast vous est OFERT ! Ce pack exclusif vous permettra de diffuser le contenu de votre NAS directement sur votre TV, en toute simplicité.",
+"description" =>"Préparez-vous à savourer un festival poignant de contenu multimédia en haute définition. Le Synology DiskStation DS216play est doté du transcodage vidéo 4K Ultra HD (3840 x 2160) en temps réel, et prend donc en charge les tous derniers contenus et formats multimédias. Le DS216play est également un NAS doté de nombreuses fonctionnalités avec un processeur double coeur et 1 Go de RAM, proposant ainsi des vitesses de 107 Mo/s en lecture et 91 Mo/s en écriture en configuration RAID 1, dans un environnement Windows. Les utilisateurs peuvent désormais profiter d'un transcodage vidéo et d'un partage de fichiers de très haute qualité à la demande offert exclusivement par le DS216play.<br /> Le DS216play vous aide à gérer facilement vos contenus multimédias et à les partager sur les plateformes Windows, Mac et Linux. Avec Video Station, vous pouvez organiser votre bibliothèque vidéo numérique avec des informations multimédias complètes, regarder des films en 4K ultra HD ou enregistrer vos émissions préférées de télévision à l'aide d'une clé DVB-T/DVB-S. Le design intuitif de Photo Station vous permet de classer sans efforts des photos en catégories personnalisées, albums intelligents et contributions de blogs, et d'établir des liens vers ceux-ci sur des sites Web de réseau social en quelques clics. Audio Station est fourni avec un plug-in intégré de gestion des paroles, la radio Internet et la prise en charge de formats audio sans perte de qualité et propose la lecture de musique via des périphériques DLNA, AirPlay et Bluetooth (Un dongle Bluetooth est nécessaire.).",
+"source" =>"img/produit5.jpg",
+"prix" => "269€95"],
+
+["name"=> "PC LDLC ZenThree",
+"accroche" => "Le PC LDLC ZenThree est une machine polyvalente avec des composants de premier choix sélectionnés pour leur haute compatibilité, leur performance et leur fiabilité. Le boitier intègre un processeur AMD Ryzen 3 1200 Wraith Stealth Edition (3.1 GHz) et 4 Go de RAM DDR4.","accroche" =>"Le PC LDLC ZenThree est une machine polyvalente avec des composants de premier choix sélectionnés pour leur haute compatibilité, leur performance et leur fiabilité. Cette configuration performante et évolutive répondra à vos attentes pour un rapport qualité/prix idéal. Le boitier design et sobre intègre un processeur AMD Ryzen 3 1200 Wraith Stealth Edition (3.1 GHz) et 4 Go de RAM DDR4.<br /> Un processeur puissant est un aubaine pour les joueurs et utilisateurs ultra-exigeants à la recherche de performances et d'efficacité. Les processeurs AMD Ryzen sont conçus pour satisfaire ces exigeances. Ils sont puissants, disposent de nombreux coeurs, d'un cache élevé et d'une consommation électrique modérée. Et pour répondre à la moindre sollicitation, la technologie AMD Turbo Core poussera la fréquence des cœurs au maximum lorsque vous en aurez le plus besoin ! Vous l'aurez compris, AMD Ryzen est un processeur nouvelle génération ultra-puissant et conçu pour TOUT faire..",
+"source" =>"img/produit6.jpg",
+"prix" => "399€95"],
+
+];
+?>
