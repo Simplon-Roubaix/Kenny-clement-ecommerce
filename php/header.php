7db5a741507889b088c990e436d0175c10450a37
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-09-07 13:59:37 +0200

    change style of header

--- php/header.php
+++ php/header.php
@@ -1,8 +1,8 @@
-<header class="d-flex row">
+<header class="d-flex ">
   <section class="col-2 ">
     <h1 >Kenny & Clément</h1>
     <img class="w-100" src="img/logo.png" alt="logo">
   </section>
   <input type="text" name="" value="" placeholder="recherche" class="col-6 ">
-<p class="col-3 hidden-xs-down"> <?php echo date("H : i"); ?> </p>
+<p class="col-1 hidden-xs-down"> <?php echo date("H : i"); ?> </p>
 </header>
