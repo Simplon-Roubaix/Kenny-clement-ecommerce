559b169b1191652dfea76e18dc34de481d4a1848
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-09-07 16:31:29 +0200

    make style of header in main.css

--- php/header.php
+++ php/header.php
@@ -1,7 +1,7 @@
-<header class="d-flex ">
-  <section class="col-2 ">
+<header class="d-flex bg-info">
+  <section class="col-5 col-md-3 ">
     <h1 >Kenny & Clément</h1>
-    <img class="w-100" src="img/logo.png" alt="logo">
+    <img class="w-75" src="img/logo.png" alt="logo">
   </section>
   <input type="text" name="" value="" placeholder="recherche" class="col-6 ">
 <p class="col-1 hidden-xs-down"> <?php echo date("H : i"); ?> </p>
