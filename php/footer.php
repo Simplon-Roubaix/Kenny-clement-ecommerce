cf0f53ab019a619b230cac24b82f4e1645ff3b11
Kenny <kennygenly@gmail.com>
2017-09-09 23:22:46 +0200

    first recording: footer

---
+++ php/footer.php
@@ -0,0 +1,55 @@
+<footer>
+
+
+	<div class="footerdiv">
+
+
+	<h6>Débit à l'expédition</h6>
+		<img class="one" src="img/cone.png">
+		<p style="color: blue; font-size: 12px;">Nous ne prélevons pas votre carte
+		bancaire avant la<br> préparation de votre<br>
+ 		commande</p>
+
+ 	<h6>Paiement en 3 fois</h6>
+		<img class="two" src="img/ctwo.png">
+		<p style="color: blue; font-size: 12px;">Par carte bancaire à partir de 100<br>
+ 		euros TTC</p>	
+
+ 	<h6>Livraison outre-mer</h6>
+		<img class="three" src="img/cthree.png">
+		<p style="color: blue; font-size: 12px;">Nous livrons dans les DOM-TOM en<br>
+ 		HT</p>	
+
+ 	<h6>Livraison Express</h6>
+		<img class="four" src="img/cfour.png">
+		<p style="color: blue; font-size: 12px;">Livré demain avant 13h</p>	
+	
+	</div>
+
+	<div class="divul">
+
+    	<ul>
+	    	<a href="#"><li>Nos services</li></a>
+	    	<a href="#"><li>Retourner un article</li></a>
+	   		<a href="#"><li>Modes de livraison</li></a>
+	    	<a href="#"><li>CGV</li></a>
+    	</ul>
+
+    	<ul>
+	    	<a href="#"><li>Mentions légales</li></a>
+	    	<a href="#"><li>Retourner un article</li></a>
+	    	<a href="#"><li>Modes de livraison</li></a>
+	    	<a href="#"><li>CGV</li></a>
+   	 	</ul>
+
+    	<ul>
+	    	<a href="#"><li>Recrutement</li></a>
+	    	<a href="#"><li>Retourner un article</li></a>
+	    	<a href="#"><li>Modes de livraison</li></a>
+	    	<a href="#"><li>CGV</li></a>
+    	</ul>
+
+    </div>
+
+
+ </footer>
